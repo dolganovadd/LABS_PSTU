3d6926fc970544e16ea517baea73acf0dddb89af
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:14:44 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_7.1.gitkeep/2_7.cpp
@@ -0,0 +1,17 @@
+﻿#include <cmath>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, s;
+    cin >> n;
+    s = sqrt(n);
+    for (int i = 0; i < s; i++)
+    {
+        for (int j = 0; j < s; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
