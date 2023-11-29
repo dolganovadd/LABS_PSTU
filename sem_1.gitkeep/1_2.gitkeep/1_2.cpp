0b62f0a1f21ae42b0bddc707579aa976802ebfc3
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:09:21 +0500

    Add files via upload

---
+++ sem_1.gitkeep/1_2.gitkeep/1_2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float a, b, res;
+	cin >> a >> b;
+	res = a / b;
+	if (b != 0)
+	{
+		cout << res;
+	}
+	else
+	{
+		cout << "no result";
+	}
+
+	return 0;
+}
