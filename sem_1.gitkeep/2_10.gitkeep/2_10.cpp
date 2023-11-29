ea3fb22143f722b43df57110ac3acf539f9ef93b
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:17:11 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_10.gitkeep/2_10.cpp
@@ -0,0 +1,19 @@
+﻿﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, n, max = 0;
+    cout << "Введите количество элементов в последовательности ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        cout << "Введите элемент последовательности ";
+        cin >> n;
+        if (n > max)
+        {
+            max = n;
+        }
+    }
+    cout << "Максимальный элемент равен " << max;
+    return 0;
+}
