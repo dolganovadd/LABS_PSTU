82691b348d870f9a7743c98c3fa02052ffa02cd1
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:16:27 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_8.gitkeep/2_8.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int spaces = n / 2;
+	int stars = 1;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < spaces; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < stars; j++)
+		{
+			cout << "*";
+		}
+		stars++;
+		cout << endl;
+	}
+	return 0;
+}
