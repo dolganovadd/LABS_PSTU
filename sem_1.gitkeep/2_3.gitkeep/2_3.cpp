cbea1e1d00aac914c807ec944de51b44b0a4700c
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:12:20 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_3.gitkeep/2_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>  
+using namespace std;
+int main() {
+    int n;
+    int tmp, sum = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        tmp = 1;
+        for (int j = 1; j <= 2 * i; j++)
+        {
+            tmp *= j;
+        }
+        sum += tmp;
+    }
+    cout << sum << endl;
+}
