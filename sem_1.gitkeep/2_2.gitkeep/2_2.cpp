34eb2213336ab38a5424cf059f1b324b51e3badf
dolganovadd <146879476+dolganovadd@users.noreply.github.com>
2023-11-29 10:11:43 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_2.gitkeep/2_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>  
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int N, b;
+    int sum = 1;
+    cout << "Введите количество чисел которые нужно умножить: ";
+    cin >> N;
+    for (int count = 1; count <= N; count++)
+    {
+        cout << "Введите число: ";
+        cin >> b;
+        sum *= b;
+    }
+    cout << sum<<endl;
+}
