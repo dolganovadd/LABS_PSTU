d957263212547bd7108b1012423d81c90de52c5d
ди <146879476+dolganovadd@users.noreply.github.com>
2023-11-30 00:03:33 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_5.gitkeep/2_5.cpp
@@ -0,0 +1,33 @@
+﻿#include <locale.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n <= 2)
+	{
+		setlocale(LC_ALL, "Russian");
+		cout << "число не соответсвует требованиям" << endl;
+	}
+	else
+	{
+		int stars = 1;
+		int spaces = n - 1;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 0; j < spaces; j++)
+			{
+				cout << " ";
+			}
+			spaces--;
+			for (int j = 0; j < stars; j++)
+			{
+				cout << "*";
+			}
+			stars += 1;
+			cout << endl;
+		}
+	}
+	return 0;
+}
