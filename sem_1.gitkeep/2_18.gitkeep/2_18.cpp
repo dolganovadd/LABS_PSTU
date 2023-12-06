0b6145f233d4f37ba0c29ecd047cc0ae76546d17
ди <146879476+dolganovadd@users.noreply.github.com>
2023-12-06 12:52:26 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_18.gitkeep/2_18.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    int r = 0; 
+
+    cin >> n;
+
+    while (n > 0)
+    {
+        r = r * 10 + n % 10;
+        n /= 10;
+    }
+    cout << r << endl;
+}
