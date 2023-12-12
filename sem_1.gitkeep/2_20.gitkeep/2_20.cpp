4ffac0c0176aa60dde5fae669be7162dc9102a98
ди <146879476+dolganovadd@users.noreply.github.com>
2023-12-12 23:03:00 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_20.gitkeep/2_20.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    float n, s, a;
+    int i = 1;
+    bool flag = false;
+
+    cin >> n >> s;
+
+    while (i <= n && !flag)
+    {
+        a = sin(n + i / n);
+        a = round(a * 10) / 10;
+        cout << n + i / n << "|" << a << endl;
+        if (a == s)
+        {
+            flag = true;
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    if (flag)
+    {
+        cout << "Элемент найден" << endl;
+    }
+    else
+    {
+        cout << "Элемент не найден" << endl;
+    }
+}
