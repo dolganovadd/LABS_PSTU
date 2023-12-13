a9a3974b537f23e71d677225fae4261837875d52
ди <146879476+dolganovadd@users.noreply.github.com>
2023-12-13 18:29:24 +0500

    Add files via upload

---
+++ sem_1.gitkeep/2_21.gitkeep/2_21.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RU");
+	int  tmp, next;
+	bool ordered=true;
+	cout << "введите число"<<endl;
+	cin >> tmp;
+	while (tmp != 0) {
+		cout << "введите след число"<<endl;
+		cin >> next;
+			if (next != 0 && next < tmp) {
+				ordered = false;
+			}
+		tmp = next;
+	}
+	if (ordered) {
+		cout << "числа упорядочены по возрастанию" << endl;
+	}
+	else {
+		cout << "числа не упорядочены по возрастанию" << endl;
+	}
+
+}
