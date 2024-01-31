0d8d683edb4e380aa0a7aad308c64d3588bd236b
ди <146879476+dolganovadd@users.noreply.github.com>
2024-01-31 19:49:13 +0500

    Add files via upload

--- sem_2.gitkeep/3_4.gitkeep/3_4.cpp
+++ sem_2.gitkeep/3_4.gitkeep/3_4.cpp
@@ -1,1 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	const int n=5;
+    bool flag = true;
+    int arr[n] = { 23, 45, 21, 67, 90 };
 
+    for (int i = n - 1; i > 0; i--) {
+        if (arr[i] < arr[i - 1]) {
+            flag = false;
+        }
+        }
+
+    if (flag) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+    return 0;
+}
