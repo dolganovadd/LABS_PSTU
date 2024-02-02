310674a68dde3245ffa4d4258baee0c91909a76e
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-02 17:17:51 +0500

    Add files via upload

--- sem_2.gitkeep/3_7.gitkeep/3_7.cpp
+++ sem_2.gitkeep/3_7.gitkeep/3_7.cpp
@@ -1,1 +1,18 @@
-
+﻿#include <iostream>
+using namespace std;
+int main() {
+	const int n = 5;
+	int arr[n] = { 32, 43, 56, 12, 56 };
+	int max = arr[0];
+	int min = arr[0];
+	for (int i = 0; i < n; i++) {
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+		else if (arr[i] < min) {
+			min = arr[i];
+		}
+	}	
+	cout << "max=" << max << endl;
+	cout << "min=" << min << endl;
+}
