f8eaab303406a254846f16ccc9ce2b61aed29d6f
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-02 17:32:37 +0500

    Add files via upload

--- sem_2.gitkeep/3_8.gitkeep/3_8.cpp
+++ sem_2.gitkeep/3_8.gitkeep/3_8.cpp
@@ -1,1 +1,22 @@
-
+﻿#include <iostream>
+using namespace std;
+int main() {
+	const int n = 5;
+	int m = n;
+	int arr[n] = { 32, 43, 56, 12, 27};
+	for (int i = 0; i < n; i++) {
+		if (arr[i] % 2 == 0) {
+			for (int j = n - 1; j < i-1; j--) {
+				arr[j+1] = arr[j];
+			}
+		
+		}
+		arr[i] = -1;
+		i++;
+		m++;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
