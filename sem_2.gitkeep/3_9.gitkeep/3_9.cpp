1fb242066bf323d19b9892bf4ec7ce68d7144a6e
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-02 17:55:40 +0500

    Add files via upload

--- sem_2.gitkeep/3_9.gitkeep/3_9.cpp
+++ sem_2.gitkeep/3_9.gitkeep/3_9.cpp
@@ -1,1 +1,38 @@
+﻿#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int n;
+
+    int a[100];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 100;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 2 != 0)
+        {
+            for (int j = i; j < n - 1; j++)
+            {
+                a[j] = a[j + 1];
+            }
+            i--;
+            n--;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
