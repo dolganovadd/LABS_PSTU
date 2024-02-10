8c0cb5da3d4f77b606a0d7bee6aca0752c5b1029
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-10 14:23:07 +0500

    Add files via upload

--- sem_2.gitkeep/labs.gitkeep/recursions.gitkeep/fibonacci.cpp
+++ sem_2.gitkeep/labs.gitkeep/recursions.gitkeep/fibonacci.cpp
@@ -1,1 +1,15 @@
+﻿#include <iostream>
 
+using namespace std;
+
+int f(int n) {
+    if (n <= 1) return n;
+    else return f(n - 1) + f(n - 2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; cout << f(i), i++);
+
+}
