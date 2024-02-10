602c62a476759763b879e26c3d7586d019095288
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-10 14:10:34 +0500

    Add files via upload

---
+++ sem_2.gitkeep/labs.gitkeep/two-dimensional arrays.gitkeep/2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int str_arr, col_arr, cnt;
+    cin >> str_arr >> col_arr >> cnt;
+    int array[str_arr][col_arr];
+
+    int num[cnt];
+    for (int i = 0; i < cnt; i++) {
+        num[i] = i + 1;
+    }
+
+    for (int i = 0; i < str_arr; i++) {
+        for (int j = 0; j < col_arr; j++) {
+            array[i][j] = 0;
+        }
+    }
+
+    for (int i = 0, k = 0; i < str_arr; k++, i++) {
+        for (int n = 0; n < col_arr - k; n++) {
+            array[i + n][n] = num[k % cnt];
+        }
+    }
+    for (int i = 0; i < str_arr; cout << endl, i++)
+        for (int j = 0; j < col_arr; cout << array[i][j] << " ", j++);
+
+    return 0;
+}
