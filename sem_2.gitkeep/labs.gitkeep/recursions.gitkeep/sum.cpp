0a36fc9ddfed7a41db44ba3b786590e5723d3d2c
ди <146879476+dolganovadd@users.noreply.github.com>
2024-02-12 15:22:45 +0500

    Add files via upload

--- sem_2.gitkeep/labs.gitkeep/recursions.gitkeep/sum.cpp
+++ sem_2.gitkeep/labs.gitkeep/recursions.gitkeep/sum.cpp
@@ -1,1 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
 
+int fact(double n) {
+    if (n <= 1) return 1;
+    else return n * fact(n - 1);
+}
+
+double sum(double n, double x) {
+    const double PI = 3.141592653589793;
+    if (n == 0) return 1;
+    else return  ((cos((n*x)) / fact(n))) + sum(n - 1, x);
+}
+
+int main() {
+    double n, x, k = 0;
+    cin >> n >> x;
+    cout << sum(n, x) << endl;
+}
