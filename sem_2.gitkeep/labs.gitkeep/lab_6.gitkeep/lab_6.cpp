e82308430964f6f4540bd76f9488b6df6943623c
ди <146879476+dolganovadd@users.noreply.github.com>
2024-03-19 22:50:17 +0500

    Add files via upload

---
+++ sem_2.gitkeep/labs.gitkeep/lab_6.gitkeep/lab_6.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+using namespace std;
+
+bool onlyNumbers(const string& str) {
+	return all_of(str.begin(), str.end(), isdigit);
+}
+
+bool onlyLetters(const string& str) {
+	return all_of(str.begin(), str.end(), isalpha);
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	string str, word;
+	string numbersOnly, lettersOnly, mixedWords;
+	cout <<"Введите строку" << endl;
+	getline(cin, str);
+	istringstream iss(str);
+	while (iss >> word) {
+		if (onlyNumbers(word)) {
+			numbersOnly += word + " ";
+		}
+		else if (onlyLetters(word)) {
+			lettersOnly += word + " ";
+		}
+		else {
+			mixedWords += word + " ";
+		}
+	}
+	string result;
+	result = numbersOnly + lettersOnly + mixedWords;
+	cout << result;
+
+}
