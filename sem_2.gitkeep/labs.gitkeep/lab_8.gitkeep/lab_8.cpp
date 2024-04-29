8fc6d0c67bb3174ecafa489c8941444fb10c3334
ди <146879476+dolganovadd@users.noreply.github.com>
2024-04-29 16:17:00 +0300

    Add files via upload

---
+++ sem_2.gitkeep/labs.gitkeep/lab_8.gitkeep/lab_8.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+#include<stdio.h>
+
+using namespace std;
+
+struct Videotape {
+	string film;
+	string director;
+	string time;
+	int price;
+};
